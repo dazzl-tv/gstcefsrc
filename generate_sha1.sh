5315741b5c4010a45f2a6d5c62609804eb524c96
Thierry GAYET <t.gayet@cyim.com>
2022-08-19 17:20:07 +0200

    upgrading the sha1 generator for cef

--- generate_sha1.sh
+++ generate_sha1.sh
@@ -2,8 +2,15 @@
 #
 # usage : ./generate_sha1.sh <FILE>
 #
-# 	Ex : ./generate_sha1.sh cef_binary_103.0.12+g8eb56c7+chromium-103.0.5060.134_linux64.tar.bz2
+# 	Ex : ./generate_sha1.sh cef_binary_104.4.18+g2587cf2+chromium-104.0.5112.81_linux64.tar.bz2
 #
+
+if [ "$#" != "1" ]; then
+	echo "Missing mandatory filename !"
+    echo "Usage : ./generate_sha1.sh <DAZZL_CEF_BZ2_TARBALL>"
+	echo "   Ex : ./generate_sha1.sh cef_binary_104.4.18+g2587cf2+chromium-104.0.5112.81_linux64.tar.bz2"
+	exit
+fi
 TARBALL="$1"
 if [ -e "${TARBALL}" ]; then
 	echo " - Generating sha1 for the tarball provided ${TARBALL}..."
@@ -12,6 +19,7 @@ if [ -e "${TARBALL}" ]; then
 	echo "RET=${RET}"
 	if [ "${RET}" == "0" ] && [ -e "${TARBALL}.sha1" ]; then
 		echo "The sha1 have been well generated!"
+		echo "-> ${TARBALL}.sha1"
 		ls -alh "${TARBALL}.sha1"
 		file    "${TARBALL}.sha1"
 		od -c   "${TARBALL}.sha1"
